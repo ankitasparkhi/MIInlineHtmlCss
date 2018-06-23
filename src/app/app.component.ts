464400bb65cec7e87177c4590f9672a717de510e
Shaileshnparkhi <shaileshnparkhi@gmail.com>
2018-06-23 15:05:49 +0530

    html and css are inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: (`<div style="text-align:center">
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
+</div>
+<label>Name:</label>
+<input type="text" size="50" placeholder="Enter your text here">`),
+  styles:[`input{color: blue}`]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Marvellous Infosystems';
 }
